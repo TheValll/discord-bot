bffb9f88799e6a1e461ae4853d94f163796b01e3
Valentin Massonniere <vmassonniere@adloox.com>
2024-10-22 18:54:55 +0200

    change rate of skynoz game and edit leaderboard]

--- skynoz_game.py
+++ skynoz_game.py
@@ -3,7 +3,7 @@ import random
 
 def game():
     # Players
-    users = ["Baptiste", "Brian", "Xarwin", "Stoaker", "Mael", "Alix", "Val", "Trytox", "Weebzard", "Mirio", "Kuzuha"]
+    users = ["Baptiste", "Brian", "Xarwin", "Stoaker", "Mael", "Alix", "Val", "Trytox", "Weebzard", "Mirio", "Kuzuha" , "Lightingloyz", "OOOOOW MY GOD"]
 
     weighted_random_index = random.randint(0, len(users) - 1)
 
@@ -11,7 +11,8 @@ def game():
     winner = users[weighted_random_index]
 
     # Open leaderboard
-    with open('/home/valentin78_massonniere/discord-bot/leaderboard.json', 'r') as file:
+    # with open('/home/valentin78_massonniere/discord-bot/leaderboard.json', 'r') as file:
+    with open('leaderboard.json', 'r') as file:
         data = json.load(file)
 
     # Edit leaderboard
@@ -21,8 +22,12 @@ def game():
     new_leaderboard = json.dumps(data)
 
     # Edit leaderboard
-    with open("/home/valentin78_massonniere/discord-bot/leaderboard.json", "w") as outfile:
+    # with open("/home/valentin78_massonniere/discord-bot/leaderboard.json", "w") as outfile:
+    with open("leaderboard.json", "w") as outfile:
         outfile.write(new_leaderboard)
 
     # Return leaderboard
     return data
+
+for i in range(35):
+    game()
