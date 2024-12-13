b03c7bf7bf84acbfc44e475273e196a5cdd383a8
Valentin Massonnière <81711481+TheValll@users.noreply.github.com>
2024-12-14 00:04:30 +0100

    Update style.py

--- style.py
+++ style.py
@@ -21,6 +21,4 @@ def format_game_results(game_results, name, date, winner, total_points, percenta
     else:
         message += f"🐐 {winner} is the gifted of the day with {total_points} point(s) !\n"
     message += f"🎂 Next birthday : {name} the {date}\n"
-    message += f"Test pour voir si les clips marchent LOL\n"
-    message += f"https://cdn.discordapp.com/attachments/1317136815060160584/1317136834525921330/trhrthr.mp4?ex=675d96c4&is=675c4544&hm=c98fd48ed15108e20cd2a8cb1c34442ed5f49d58df76cc5326db1c9f75b7fb95&\n"
     return message
